a8619efbf1a6ed721b3ac599c8b4b7808f7920f7
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-21 20:42:07 +0200

    refactor: replace Link with NavLink in Header component for active link styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="bg-slate-800">
-        <div className="mx-auto container px-5 py-16">
-            <div className="flex justify-between items-center">
-                <div>
-                    <img src="/logo.svg" alt="logotipo" className="w-32" />
-                </div>
+      <div className="mx-auto container px-5 py-16">
+        <div className="flex justify-between items-center">
+          <div>
+            <img src="/logo.svg" alt="logotipo" className="w-32" />
+          </div>
 
-                <nav className="flex gap-4">
-                    <Link to="/" className="text-white uppercase font-bold">Inicio</Link>
-                    <Link to="/favoritos" className="text-white uppercase font-bold">Favoritos</Link>
-                </nav>
-            </div>
+          <nav className="flex gap-4">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Inicio
+            </NavLink>
+            <NavLink
+              to="/favoritos"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Favoritos
+            </NavLink>
+          </nav>
         </div>
+      </div>
     </header>
-  )
+  );
 }
