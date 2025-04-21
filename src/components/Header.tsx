f7a86f3acf642662c119a3c924de97f5a9483328
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-21 21:08:39 +0200

    feat: add ingredient search form to Header component for home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+
+  const isHome = useMemo(() => location.pathname === "/", [location.pathname]);
+
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
@@ -32,6 +37,52 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+
+        {isHome && (
+          <form
+            action=""
+            className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
+          >
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o ingredientes
+              </label>
+
+              <input
+                type="text"
+                name="ingredient"
+                id="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                placeholder="Nombre o ingrediente. Ej. Vodka, tequila, café ..."
+              />
+            </div>
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoría
+              </label>
+
+              <select
+                name="ingredient"
+                id="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+              >
+                <option value="">-- Seleccione --</option>
+              </select>
+            </div>
+
+            <input
+              value="Buscar recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+              type="submit"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
