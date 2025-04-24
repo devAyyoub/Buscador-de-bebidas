b2ff590cc77ed2b853d985b0fd25c8bf6e47d379
Ayyoub <84755795+devAyyoub@users.noreply.github.com>
2025-04-25 01:34:22 +0200

    feat: add AI slice and integrate with useAppStore for recipe management

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,11 +3,13 @@ import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { createFavoriteSlice, FavoritesSliceType } from "./favoritesSlice";
 import { createNotificationSlice, NotificationSliceType } from "./notificationSlice";
+import { AISlice, createAISlice } from "./aiSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
     ...createFavoriteSlice(...a),
     ...createNotificationSlice(...a),
+    ...createAISlice(...a),
   }))
 );
